d56d3afa0124c4116c5a2580d4bc5679a0a8cf2e
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2017-09-19 09:08:55 -0600

    Used scanner and else if.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -13,7 +13,7 @@ public class MonsterController
 		MarshmallowMonster realMonster = new MarshmallowMonster("Good Ol' NoName", 8, 1, 4.5, true);
 		
 		System.out.println(realMonster);
-		System.out.println("Preston is hungry, so he is going to eat a tentac");
+		System.out.println("Preston is hungry, so he is going to eat a tentacle");
 		realMonster.setTentacleAmount(3.5);
 		System.out.println(realMonster);
 		
@@ -27,8 +27,36 @@ public class MonsterController
 		System.out.println("How many do you want to eat?");
 		Scanner myScanner = new Scanner(System.in);
 		int consumed = myScanner.nextInt();
-		currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
-		System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getArmCount() + " arms left.");
 		
+		if(consumed < 0)
+		{
+			System.out.println("You cannot eat a negative amount silly human");
+		}
+		else if(consumed == 0)
+		{
+			System.out.println("Not that hungry are you?");
+		}
+		else if(consumed > currentMonster.getArmCount())
+		{
+			System.out.println("That is impossible - I only have " + currentMonster.getArmCount() + " arms!!!");
+		}
+		else
+		{
+			currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
+			System.out.println("Thank you so much! I only have this many arms now: " + currentMonster.getArmCount());
+		}	
+		if(consumed < 0)
+		{
+			System.out.println("Cant eat that sorry");
+		}
+		else if(consumed == 0)
+		{
+			System.out.println("Methinks you are not hungry");
+		}
+		else
+		{
+			currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
+			System.out.println("Thanks! I only have this many eyes now: " + currentMonster.getEyeCount());
+		}
 	}
 }
