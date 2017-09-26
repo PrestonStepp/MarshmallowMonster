6cb5f1c082c5e06cbb0047bbaa0b4368de96efb1
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2017-09-26 09:17:58 -0600

    Added some loops.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -15,6 +15,19 @@ public class MonsterController
 	
 	public void start()
 	{
+		boolean finished = true;
+		int count = 0;
+//		while(count < 100)
+//		{
+//			popup.displayText("I am so neat! " + count);
+//			count += 1;
+//		}
+//		
+		for(int loop = 0; loop < 15; loop += 1)
+		{
+			popup.displayText("I am looping " + (loop + 1) + " times out of 15");
+		}
+		
 		MarshmallowMonster sample = new MarshmallowMonster();
 //		System.out.println(sample);
 		popup.displayText(sample.toString());
@@ -39,11 +52,17 @@ public class MonsterController
 //		System.out.println("How many do you want to eat?");
 		int specialAnswer;
 		String unconverted = popup.getResponse("How many do you want to eat?");
-		if(isValidInteger(unconverted))
+		
+		while(! isValidInteger(unconverted)) 
 		{
-			specialAnswer = Integer.parseInt(unconverted);
+			popup.displayText("Try again!");
+			unconverted = popup.getResponse("How many arms?????");
 		}
 		
+//		if(isValidInteger(unconverted))
+//		{
+			specialAnswer = Integer.parseInt(unconverted);
+//		}
 		
 		Scanner myScanner = new Scanner(System.in);
 		int consumed = myScanner.nextInt();
